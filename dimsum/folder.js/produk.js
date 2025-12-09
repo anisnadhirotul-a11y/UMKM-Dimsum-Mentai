1555a504a7b042d9654286e854463d2194f2619e
Anis <anisnadhirotul@gmai.com>
2025-12-09 16:01:24 +0700

    inisialisasi projek

---
+++ dimsum/folder.js/produk.js
@@ -0,0 +1,4 @@
+import { formatHarga } from "./utils.js"; 
+
+const harga = 12000; 
+console.log(formatHarga(harga)); 
---
+++ dimsum/folder.js/script.js
@@ -0,0 +1,9 @@
+alert('Selamat datang di Website Profil UMKM Dimsum Mentai Premium!');
+
+const namaUMKM = 'Dimsum Mentai Premium';
+console.log('Nama UMKM:', namaUMKM);
+
+let produk = 3;
+console.log('Jumlah produk saat ini:', produk);
+produk += 2;
+console.log('Setelah penambahan, total produk:', produk);
